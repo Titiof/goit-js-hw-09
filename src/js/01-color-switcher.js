c8b3c417f4393d766e75f9c982ee99dec87ed04d
Rumberg Vitalii <modtitiof@gmail.com>
2023-09-13 17:03:37 +0300

    check task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyElement: document.body,
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16716215).toString(16).padStart(6, 0)}`;
+};
+
+let colorId;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+    colorId = setInterval(() => { refs.bodyElement.style.backgroundColor = getRandomHexColor() }, 1000);
+    refs.startBtn.disabled = true;
+
+}
+
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStopBtnClick() {
+    clearInterval(colorId);
+     refs.startBtn.disabled = false;
+}
